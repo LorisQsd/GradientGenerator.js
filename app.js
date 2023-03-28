d0e9ce2ecc8b17dd3c4404925938ae278a949ed4
LorisQ <121116287+LorisQsd@users.noreply.github.com>
2023-03-28 10:23:18 +0200

    CSS / JS improved
    - Label : Color of font size changes between black & white depending of luminosity (yiq)
    - Add a copy toast by clicking the #copy-btn
    - Once the #random-btn affects a new color, the input type color adapts to this new color
    - Add a cursor pointer on labels color

--- app.js
+++ app.js
@@ -24,28 +24,60 @@ function changeBg(){
 
     document.querySelector(".input-group:nth-child(1)").style.backgroundColor = color1.value;
     document.querySelector(".input-group:nth-child(2)").style.backgroundColor = color2.value;
+    adaptInputsColor();
 }
 
 const copyBtn = form.querySelector("#copy-btn");
 copyBtn.addEventListener("click", copyGradient);
-
+let lock = false;
 function copyGradient(){
     navigator.clipboard.writeText('background: linear-gradient('
     + orientationInput.value + 'deg, ' + color1.value + ', ' + color2.value + ')')
+
+    if(lock) return;
+
+    lock = true;
+    copyBtn.classList.add("active")
+  
+    setTimeout(() => {
+    copyBtn.classList.remove("active")
+    lock = false;
+    }, 1000)
 }
 
 const randomBtn = form.querySelector("#random-btn")
 randomBtn.addEventListener("click", generateColor)
 
 function generateColor(){
-    random1 = "#" + Math.floor(Math.random()*16777215).toString(16);
-    random2 = "#" + Math.floor(Math.random()*16777215).toString(16);
+    color1.value = random1 = "#" + Math.floor(Math.random()*16777215).toString(16);
+    color2.value = random2 = "#" + Math.floor(Math.random()*16777215).toString(16);
 
     bodyBg.style.backgroundImage = 'linear-gradient('
     + orientationInput.value + 'deg, ' + random1 + ', ' + random2 + ')';
+
     label1.textContent = random1;
     label2.textContent = random2;
     
     document.querySelector(".input-group:nth-child(1)").style.backgroundColor = random1;
     document.querySelector(".input-group:nth-child(2)").style.backgroundColor = random2;
 }
+
+function adaptInputsColor(){
+    const colorLabels = [...document.querySelectorAll(".input-group label")]
+    colorLabels.forEach(label => {
+      const hexColor = label.textContent.replace("#", "");
+      const red = parseInt(hexColor.slice(0,2), 16)
+      const green = parseInt(hexColor.slice(2,4), 16)
+      const blue = parseInt(hexColor.slice(4,6), 16)
+  
+      const yiq = (red * 299 + green * 587 + blue * 144) / 1000;
+      console.log(yiq);
+   
+      if(yiq >= 128) {
+        label.style.color = "#111"
+      }
+      else {
+        label.style.color = "#f1f1f1"
+      }
+    })
+  }
